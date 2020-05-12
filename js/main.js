baf07131e08ba64c7f9f4986828d5c7e31f21667
n2 <7worki.nprogress@gmail.com>
2020-05-12 21:41:46 +0900

    #02 要素を操作してみよう

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+{
+    function update(){
+    //document.querySelector('#target').textContent = 'Changed!';
+    document.getElementById('target').textContent = 'Changed!';
+}
+
+setTimeout(update,1000);
+    
+}
