ea5c227673049e495d76976e6621432cc6b2dafe
n2 <7worki.nprogress@gmail.com>
2020-05-13 15:39:44 +0900

    #17 いろいろなイベントを見てみよう

--- js/main.js
+++ js/main.js
@@ -1,25 +1,11 @@
 'use strict';
 {
-// ＤＯＭから要素を削除しよう！
-
-document.querySelector('button').addEventListener('click',() =>{
-    const colors = document.querySelectorAll('input');
-    const selecterColors = []; //複数選択OKなので配列にしておく
-
-    colors.forEach(color=> {
-        if( color.checked === true){
-            selecterColors.push(color.value);
-        }
+    document.querySelector('button').addEventListener( 'dblclick' , () => {
+        console.log('Double Click!');
     });
-    const li = document.createElement('li');
-    
-    li.textContent = selecterColors.join(',');
-    document.querySelector('ul').appendChild(li);
 
-});
-
-
-
-
+    document.addEventListener('mousemove', () => {
+        console.log('moved!');
+    })
 
 }
