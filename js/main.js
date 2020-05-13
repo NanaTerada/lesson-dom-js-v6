e69e7802f4f5a91a5b2ac83a44975ee813d29942
n2 <7worki.nprogress@gmail.com>
2020-05-13 11:33:47 +0900

    #10 要素を追加してみよう

--- js/main.js
+++ js/main.js
@@ -4,10 +4,21 @@
 
 document.querySelector('button').addEventListener('click',() =>{
 
-    const targetNode = document.getElementById('target');
+    const item2 = document.createElement('li');　//liの要素をつくる
+    item2.textContent = 'item 2'; //中身のテキスト
 
-// targetNode.textContent = 'Dotinstall';
-targetNode.textContent = targetNode.dataset.tanslation;
+    // DOMツリーに追加
+
+    // const ulNode = document.querySelector('ul'); //親要素であるｕｌを取得
+    const ul = document.querySelector('ul');
+    ul.appendChild(item2);
+
+
+
+    
+
+  
+
 
 });
 
