5fe972f9a9849de129860fd8af1e710ed1955582
n2 <7worki.nprogress@gmail.com>
2020-05-13 11:54:24 +0900

    #13 input要素を操作してみよう

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@
 // ＤＯＭから要素を削除しよう！
 
 document.querySelector('button').addEventListener('click',() =>{
-    const item1 = document.querySelectorAll('li')[1];
-    // item1.remove();//　一部のブラウザで使えない場合あり、その場合は↓
-    // 親Node.removeChild(削除するNode)
-    document.querySelector('ul').removeChild(item1);
+    const li = document.createElement('li');
+    const text = document.querySelector('input');
+    li.textContent = text.value; //valueで入力された値を取得できる
+    document.querySelector('ul').appendChild(li);
 
-
+    text.value = ''; //addするたびに入力欄が空になり
+    text.focus(); //フォーカスされている状態　
+   
 });
 
 
