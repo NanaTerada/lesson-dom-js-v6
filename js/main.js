0f0544c7db344a0a4c04e9fe5475d530c9a0914c
n2 <7worki.nprogress@gmail.com>
2020-05-13 14:50:58 +0900

    #16 チェックボックスを操作してみよう

--- js/main.js
+++ js/main.js
@@ -4,18 +4,21 @@
 
 document.querySelector('button').addEventListener('click',() =>{
     const colors = document.querySelectorAll('input');
-    let selectedColor;
+    const selecterColors = []; //複数選択OKなので配列にしておく
 
-    colors.forEach( (color) => {
-        if( color.checked === true){ //要素がチェックされていたら
-            selectedColor = color.value;　//要素の値を代入する
-    }
+    colors.forEach(color=> {
+        if( color.checked === true){
+            selecterColors.push(color.value);
+        }
     });
-
     const li = document.createElement('li');
-    li.textContent = selectedColor;
+    
+    li.textContent = selecterColors.join(',');
     document.querySelector('ul').appendChild(li);
+
 });
+
+
 
 
 
